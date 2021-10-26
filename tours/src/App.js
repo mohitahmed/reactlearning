11e799792b7667ff27d3118ed44b27ab6bf6fc5c
mohitahmed <56644756+mohitahmed@users.noreply.github.com>
2021-10-26 23:13:22 +0600

    remove tours functionality

--- tours/src/App.js
+++ tours/src/App.js
@@ -7,6 +7,10 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
   const fetchTours = async () => {
     setLoading(true);
 
@@ -33,7 +37,7 @@ function App() {
   }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
